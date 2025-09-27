d006d854faf7261ee8df0accb7bdc5444e654143
Sushant sharma <atitsushant@gmail.com>
2025-09-27 12:38:15 +0530

    Initial commit for treecraze-backend

--- backend_api/apps.py
+++ backend_api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ReportsConfig(AppConfig):
+class BackendApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'reports'
+    name = 'backend_api'
